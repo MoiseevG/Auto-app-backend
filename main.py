9b4bfa221c2a28fc5af0b8724ff509aba339e511
MoiseevG <lookatthescarsonyoursoul@gmail.com>
2025-11-28 12:53:14 +0300

    Fix routes

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL, echo=True)
 
 app = FastAPI()
-app.add_route("/records", record_router)
+app.include_router(record_router, prefix="/records")
 
 app.add_middleware(
     CORSMiddleware,
