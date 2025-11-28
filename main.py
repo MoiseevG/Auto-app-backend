f21343ece44c9550a59c0ecc9c62a03be683197c
MoiseevG <lookatthescarsonyoursoul@gmail.com>
2025-11-28 12:39:21 +0300

    Fix CORS

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
 from sqlmodel import SQLModel, create_engine
-
+from api import record_router
 import os
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "https://auto-app-zovz.onrender.com",
+    "http://localhost:3000",
+]
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
 app = FastAPI()
+app.add_routeer("/records", record_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Создание таблиц при старте
 @app.on_event("startup")
